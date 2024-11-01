b0ece4fa7b9827c7279428a5e54aa9069d1202d0
Thanawat Panpinij <thanawatpanpinij@gmail.com>
2024-11-01 16:22:28 +0700

    feat: get weather and countries data

--- src/weather-app.js
+++ src/weather-app.js
@@ -1,7 +1,22 @@
 const INPUT_ELEM = document.querySelector(".search__input");
 
-function getData(city = "bangkok") {
-    console.log(city);
+function updateData(weather, countries) {
+    console.log(weather);
+    console.log(countries);
+}
+
+async function getData(city = "bangkok") {
+    const weatherApiKey = "8ffffdc1db5f000776826bad8ba16c25";
+    const weatherApi = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=th";
+    const weatherResponse = await fetch(`${weatherApi}&q=${city}&appid=${weatherApiKey}`);
+    if (weatherResponse.status === 404) return;
+    const weatherData = await weatherResponse.json();
+
+    const thCountriesApi = "../src/JSON/country-list-th.json";
+    const countries = await fetch(thCountriesApi);
+    const countriesData = await countries.json();
+
+    updateData(weatherData, countriesData);
 }
 
 function clearInput() {
